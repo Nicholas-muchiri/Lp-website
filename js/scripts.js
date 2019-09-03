b014052f416129ac0e116b55dbe3c80e8e527fa9
Mr Nobody <nickromero187@gmail.com>
2019-09-03 10:12:13 +0300

    files added

--- js/scripts.js
+++ js/scripts.js
@@ -16,4 +16,26 @@ $('body').scrollspy({
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
+});;
+
+
+
+dependencies:
+
+//cdnjs.cloudflare.com/ajax/libs/jquery.touchswipe/1.6.4/jquery.touchSwipe.min.js
+//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js
+//cdnjs.cloudflare.com/ajax/libs/jquery.touchswipe/1.6.4/jquery.touchSwipe.min.js
+
+
+
+$(".carousel").swipe({
+
+  swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+
+    if (direction == 'left') $(this).carousel('next');
+    if (direction == 'right') $(this).carousel('prev');
+
+  },
+  allowPageScroll:"vertical"
+
 });
